2a1ee7c94260ce5d5c984f569b1132c8cd62d7ae
Claudinei Almeida Souza <devdinei@gmail.com>
2025-02-28 21:11:54 -0300

    aulas 7 8

---
+++ 2_FUNDAMENTOS/7_explorando_console/index.js
@@ -0,0 +1,21 @@
+// mais de um valor
+
+const x = 10
+const y = 'Algum Testo'
+const z = [1,2]
+
+console.log(`Constante x: ${x}, constante y:${y}, constante z: ${z}`)
+
+// contagem de impressões
+
+console.count(`O valo de x é: ${x}, contagem:`)
+
+// variavel entre string
+
+console.log("o nome é %s, e ele é programador", y)
+
+// limpar console
+
+setTimeout(() =>{
+    console.clear()
+},2000)
