1d21c4063c9d9d6ef327e8c327e22e40fa60a089
Claudinei Almeida Souza <devdinei@gmail.com>
2025-03-02 15:29:47 -0300

    tentando usar o chalk

--- 2_FUNDAMENTOS/8_chalk/index.js
+++ 2_FUNDAMENTOS/8_chalk/index.js
@@ -1,5 +1,7 @@
-const chalk = require('chalk')
+const chalk = require('chalk');
+const nota = 9;
 
-const nota = 9
+// Defina o nível de suporte a cores se necessário
+chalk.level = 1;
 
-console.log(chalk.green("Parabens"))
+console.log(chalk.green("Aprovado"));
