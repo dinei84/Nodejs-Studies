9739e86276c91827dce697e7d02131e26e005ba2
Claudinei Almeida Souza <devdinei@gmail.com>
2025-08-05 23:13:36 -0300

    Adicionando Exercicio

---
+++ 6_EXPRESS/7_post_de_dados/index.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const port = 3000
+const app = express()
+
+const path = require('path')
+
+
+//ler o body
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+)
+app.use(express.json())
+
+const basePath = path.join(__dirname, 'templates')
+
+
+app.get('/users/add',(req, res) =>{
+    res.sendFile(`${basePath}/userform.html`)
+})
+
+app.post('/users/save',(req, res)=>{
+
+    console.log(req.body)
+
+})
+
+app.get('/users/:id', (req, res) => {
+    const id = req.params.id
+
+    //Leitura da tabela users, resgatar um usuario do banco
+    console.log(`Estamos buscando pelo usuario ${id}`)
+
+    res.sendFile(`${basePath}/users.html`)
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(`${basePath}/index.html`)
+})
+
+app.listen(port, ()=>{
+    console.log(`Servidor rodando na porta ${port}`)
+})
