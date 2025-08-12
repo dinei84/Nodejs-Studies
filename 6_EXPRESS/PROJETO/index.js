18b63a251c6d0bdc5e51934ddad637cdb88e5970
Claudinei Almeida Souza <devdinei@gmail.com>
2025-08-12 14:47:25 -0300

    /começado exercicio

--- 6_EXPRESS/PROJETO/index.js
+++ 6_EXPRESS/PROJETO/index.js
@@ -5,4 +5,11 @@ const path = require ('path')
 const basePath = path.join(__dirname, '../templates')
 
 
+app.get('/', (req, res) => {
+    res.sendFile(`${basePath}/index.js`)
+})
 
+
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}`)
+})
